2116e99022f5ba24c06db61af90cb075bd07d3c6
backinkansas <milena.martinsc@gmail.com>
2018-10-01 13:32:47 -0300

    Separates actions in different functions to reorganize

--- DOM-challenge/js/main.js
+++ DOM-challenge/js/main.js
@@ -1,37 +1,57 @@
 const form = document.getElementById('form-tasks')
 const taskList = document.getElementById('list-tasks')
+const classDone = 'list-item--done'
 
 function classToggle(listItem) {
-
-    if (listItem.classList.contains('list-item--done')) {
-        listItem.classList.remove('list-item--done')
-    } else {
-        listItem.classList.add('list-item--done')
-    }
-
+    listItem.addEventListener('click', function(){
+        if (listItem.classList.contains(classDone)) {
+            listItem.classList.remove(classDone)
+        } else {
+            listItem.classList.add(classDone)
+        }
+    })
 }
 
-function modifyListAndCleanForm() {
-    const input = document.getElementById('form-text-input')
+function isValid(texto) {
+    const regex = /\S/
+    const notEmpty = regex.test(texto)
+
+    if (texto.length > 0 && notEmpty) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function buildMarkUp(input) {
     const newListItem = document.createElement('li')
     const taskContent = input.value
     newListItem.textContent = taskContent
 
-    taskList.appendChild(newListItem)
+    classToggle(newListItem)
 
+    return function addToList() {
+        taskList.appendChild(newListItem)
+    }
+}
+
+function cleanFormField() {
+    const input = document.getElementById('form-text-input')
     input.value = ''
+}
 
-    newListItem.addEventListener('click', function() {
-
-        classToggle(newListItem)
-
-    })
+function executeViewChange(input) {
+    buildMarkUp(input)()
+    cleanFormField()
 }
 
 form.addEventListener('submit', function(event) {
     event.preventDefault()
+    const input = document.getElementById('form-text-input')
 
-    modifyListAndCleanForm()
-
+    if(isValid(input.value)) {
+        executeViewChange(input)
+    } else {
+        alert('Tarefa inválida!')
+    }
 })
-
